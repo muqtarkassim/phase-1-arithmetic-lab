d7efbe459b96edaa9eed863e510be18ca019b362
muqtar <muqtarabdiaziz8@gmail.com>
2023-09-18 12:35:01 +0300

    compl;eted

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=31;
+const num2=2;
+const multiply=num1*num2;
+// Write your code h
 
-// Write your code here
+
+const random=Math.floor(Math.random()*10)+1;
+let num3=10;
+let num4=6;
+const mod=num3 % num4;
+
+const number=[10,12,3,20];
+const max=Math.max(...number)
